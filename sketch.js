c847d05c7fb3661f129cc07d99f111a385f0d6d4
hafsid <50413142+hafsid@users.noreply.github.com>
2021-09-22 15:18:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var trex ,trex_running;
+var edges;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,150,20,50);
+  trex.addAnimation("running",trex_running);
+  trex.scale = 0.5;
+
+  edges = createEdgeSprites();
+ 
+}
+
+function draw(){
+  background("lightgray")
+
+  if(keyDown("space")){
+    trex.velocityY = -10
+  }
+
+  trex.velocityY = trex.velocityY +0.5;
+
+  trex.collide(edges[3]);
+  
+  drawSprites();
+}
