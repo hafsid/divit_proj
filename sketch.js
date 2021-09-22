8f7254d2665367cee80a4c1bf997e579504b8ba8
hafsid <50413142+hafsid@users.noreply.github.com>
2021-09-22 15:19:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
-var trex ,trex_running;
+
 var edges;
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png")
+
 
 }
 
 function setup(){
   createCanvas(600,200)
   
-  //create a trex sprite
-  trex = createSprite(50,150,20,50);
-  trex.addAnimation("running",trex_running);
-  trex.scale = 0.5;
+ 
 
   edges = createEdgeSprites();
  
@@ -21,13 +18,7 @@ function setup(){
 function draw(){
   background("lightgray")
 
-  if(keyDown("space")){
-    trex.velocityY = -10
-  }
-
-  trex.velocityY = trex.velocityY +0.5;
-
-  trex.collide(edges[3]);
+ 
   
   drawSprites();
 }
